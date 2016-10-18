47be7c389fdb19cd0888c395a1651dcaa6579271
weimingtom <weimingtom@qq.com>
2016-10-18 15:10:35 +0800

    remove some files not available under android

--- src/com/iteye/weimingtom/dom4j/bean/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * An implementation of the <i>dom4j</i> API which allows JavaBeans to be used to store and retrieve attribute values from Element.
- */
-package com.iteye.weimingtom.dom4j.bean;
